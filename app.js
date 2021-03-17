15e86d2224a1b6460866805fff4098bd2847528b
Esa Mäkipää <esa.makipaa@gmail.com>
2021-03-17 09:33:33 +0200

    Exercise 11.11, ensured notification if a deployment breaks the application

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// for a health check
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// for deployed version
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
